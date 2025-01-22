18c8eee71b310045c71b246da960d16f4f02fb97
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 13:46:25 +0000

    тест бд

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
-sudo bash -c "echo -e 'local   all             all                                  trust' > /etc/postgresql/15/main/pg_hba.conf"
-sudo bash -c "echo -e 'host   $POSTGRES_DB           $POSTGRES_USER          0.0.0.0/0            md5' >> /etc/postgresql/15/main/pg_hba.conf"
+sudo apt update -y
+sudo apt install postgresql -y
 sudo systemctl start postgresql
-echo $POSTGRES_DB
 sudo -u postgres psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
 #psql -U postgres -d $POSTGRES_DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
 #psql -U postgres -d $POSTGRES_DB -c "CREATE TABLE prices(
