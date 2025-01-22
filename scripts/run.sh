6530b3838118e3e1f9c24b3914efa18ab63bc632
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 15:11:55 +0000

    тест бд

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
 sudo systemctl start postgresql
-sudo -u $POSTGRES_USER psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
+sudo cat /etc/postgresql/16/main/pg_hba.conf
+sudo -u postgres psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
 #psql -U postgres -d $POSTGRES_DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
 #psql -U postgres -d $POSTGRES_DB -c "CREATE TABLE prices(
 #                                        id INTEGER PRIMARY KEY NOT NULL,
