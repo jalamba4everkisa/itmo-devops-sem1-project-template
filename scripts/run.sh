8156793b79daeb7d2b1a1734c3142adf04e8e244
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 13:28:58 +0000

    изменил развёртывание базы postgres в run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
+systemctl start postgresql
+psql -U $POSTGRES_USER -c 'CREATE DATABASE "'$POSTGRES_DB'";'
 psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "CREATE TABLE prices(
                                         id INTEGER PRIMARY KEY NOT NULL,
                                         name VARCHAR NOT NULL,
@@ -7,4 +9,6 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "CREATE TABLE prices
                                         price REAL NOT NULL,
                                         create_date DATE NOT NULL
                                     )";
+
+psql -U $POSTGRES_USER -d $POSTGRES_DB -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
 go run . &
