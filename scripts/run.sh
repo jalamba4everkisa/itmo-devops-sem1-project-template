648d0dd6ad09b81e7663a68ac7d5c8eaad5400b4
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 15:49:38 +0000

    тест бд

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
-sudo systemctl start postgresql
-sudo systemctl status postgresql
-psql -U postgres -c 'CREATE DATABASE "'$POSTGRES_DB'";'
-#sudo -u postgres psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
-#psql -U postgres -d $POSTGRES_DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
-#psql -U postgres -d $POSTGRES_DB -c "CREATE TABLE prices(
-#                                        id INTEGER PRIMARY KEY NOT NULL,
-#                                        name VARCHAR NOT NULL,
-#                                        category VARCHAR NOT NULL,
-#                                        price REAL NOT NULL,
-#                                        create_date DATE NOT NULL
-#                                    )";
-#psql -U postgres -d $POSTGRES_DB -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
+psql -U postgres -d $POSTGRES_DB -c "CREATE TABLE prices(
+                                        id INTEGER PRIMARY KEY NOT NULL,
+                                        name VARCHAR NOT NULL,
+                                        category VARCHAR NOT NULL,
+                                        price REAL NOT NULL,
+                                        create_date DATE NOT NULL
+                                    )";
+psql -U postgres -d $POSTGRES_DB -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
 go run . &
