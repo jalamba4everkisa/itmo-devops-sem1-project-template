b21b94ce5f193d57c25bb90607c79516192e6ea4
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 13:33:02 +0000

    дополнил разворачивание постгри

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
-systemctl start postgresql
-psql -U $POSTGRES_USER -c 'CREATE DATABASE "'$POSTGRES_DB'";'
-psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "CREATE TABLE prices(
+sudo systemctl start postgresql
+sudo -u postgres psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
+sudo -u postgres psql -d $POSTGRES_DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
+sudo -u postgres psql -d $POSTGRES_DB -c "CREATE TABLE prices(
                                         id INTEGER PRIMARY KEY NOT NULL,
                                         name VARCHAR NOT NULL,
                                         category VARCHAR NOT NULL,
                                         price REAL NOT NULL,
                                         create_date DATE NOT NULL
                                     )";
-
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
+sudo -u postgres psql -d $POSTGRES_DB -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
 go run . &
