dbd0ff6d81062dc329d52df6fdddb1406b63a01f
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 18:10:56 +0000

    добавил создание .env файла

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export PGPASSWORD=$POSTGRES_PASSWORD
 sudo systemctl start postgresql
+echo "POSTGRES_USER=$POSTGRES_USER" > .env
+echo "POSTGRES_DB=$POSTGRES_DB" > .env
+echo "POSTGRES_PASSWORD=$POSTGRES_PASSWORD" > .env
+cat .env
 psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "CREATE TABLE prices(
                                         id INTEGER PRIMARY KEY NOT NULL,
                                         name VARCHAR NOT NULL,
@@ -11,4 +15,3 @@ psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "CREATE TABLE prices
 psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "GRANT SELECT, UPDATE, INSERT,DELETE ON prices TO $POSTGRES_USER;"
 psql -U $POSTGRES_USER -d $POSTGRES_DB -h $POSTGRES_HOST -c "SELECT * FROM prices;"
 ./project_sem&
-netstat -tulnp
