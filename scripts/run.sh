9b7f78a0bcf01a44085fd7b8cec26679eeb59e89
nndanchenko@yandex.ru <nndanchenko@yandex.ru>
2025-01-22 15:42:44 +0000

    тест бд

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 export PGPASSWORD=$POSTGRES_PASSWORD
 sudo systemctl start postgresql
 sudo systemctl status postgresql
-sudo psql -U postgres
+psql -U postgres postgres
 #sudo -u postgres psql -c 'CREATE DATABASE "'$POSTGRES_DB'";'
 #psql -U postgres -d $POSTGRES_DB -c "CREATE USER $POSTGRES_USER WITH PASSWORD '$POSTGRES_PASSWORD';"
 #psql -U postgres -d $POSTGRES_DB -c "CREATE TABLE prices(
